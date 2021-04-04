58ae4b4fd3a3eaa07bc6cccede00af21664ca7a9
hamza <mcsf19a025@pucit.edu.pk>
2021-04-05 00:03:48 +0500

    2nd day

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -26,7 +27,9 @@ public class MainActivity extends AppCompatActivity {
             listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+                    Intent intent=new Intent(MainActivity.this,MainActivity2.class);
+                    intent.putExtra("name",alp.get(position));
+                    startActivity(intent);
                 }
             });
         }
--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -2,14 +2,24 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity2 extends AppCompatActivity {
-
+MediaPlayer md;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        String name= getIntent().getStringExtra("name").toLowerCase();
+        int id=getResources().getIdentifier(name,"drawable",getPackageName());
+        ImageView imageView=findViewById(R.id.imageView);
+        imageView.setImageResource(id);
+        int mdp=getResources().getIdentifier(name, "raw",getPackageName());
+        md=MediaPlayer.create(getApplicationContext(),mdp);
+        md.start();
         
+
     }
 }
